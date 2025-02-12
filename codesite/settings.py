334e251e7428b47695b150b58a233fd410aff36d
Jani Pekkala <pekkala.jani@hotmail.fi>
2025-02-12 17:07:01 +0200

    settings

--- codesite/settings.py
+++ codesite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG")
 
 # SECURITY WARNING: In production, allow only those domains which you trust.
 #REST CORS ja sallitut domainit
