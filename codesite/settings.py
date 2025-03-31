dea2a3ed9204cbf75b48cee6c42bbf15a3a63f8e
Toni Pekkala <toni.pekkala@hotmail.com>
2025-03-31 23:29:45 +0300

    Lisätty sähköpostiasetukset .env tiedostoon.

--- codesite/settings.py
+++ codesite/settings.py
@@ -186,3 +186,16 @@ SIMPLE_JWT = {
 }
 
 
+
+#salasanan palautus
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("EM_PASSWORD")
+
+
+# Authentication
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
